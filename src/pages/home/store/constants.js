f94009e4ed30e155f4894305487f0c76b3702ebf
JeremyK <kangtaohan9863@gmail.com>
2019-02-22 18:42:15 -0700

    Create constants.js

---
+++ src/pages/home/store/constants.js
@@ -0,0 +1,7 @@
+export const changePlayState = 'home/changePlayState';
+export const changeCurrentTime = 'home/changeCurrentTime';
+export const changeDuration = 'home/changeDuration';
+export const changeCovered = 'home/changeCovered';
+export const getData = 'home/getData';
+export const changeMusicList = 'home/changeMusicList';
+export const changeIndex = 'home/changeIndex';
