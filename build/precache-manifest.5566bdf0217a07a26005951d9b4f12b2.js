dcd6466526d127af6e5cf3fc0aefac3230a72293
JeremyK <kangtaohan9863@gmail.com>
2019-03-05 19:47:57 -0700

    111

--- build/precache-manifest.5566bdf0217a07a26005951d9b4f12b2.js
+++ build/precache-manifest.5566bdf0217a07a26005951d9b4f12b2.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/music-react/static/js/runtime~main.6cabeb02.js"
   },
   {
-    "revision": "dcd334a0783dac73810e",
-    "url": "/music-react/static/js/main.dcd334a0.chunk.js"
+    "revision": "a7ea8bf91b649339badd",
+    "url": "/music-react/static/js/main.a7ea8bf9.chunk.js"
   },
   {
     "revision": "cb402fb134af8bd7fcc5",
     "url": "/music-react/static/js/2.cb402fb1.chunk.js"
   },
   {
-    "revision": "0f4ea554369e3c7053867a5778c6f80a",
+    "revision": "5dc46f5c2f4a7dfb8d41a5c13e8b0e77",
     "url": "/music-react/index.html"
   }
 ];
